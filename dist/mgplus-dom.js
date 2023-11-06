832d4499b25e7722599c59ecc56749557f41c2d3
medevod <medevod@github.com>
2023-11-07 00:18:39 +0100

    update demo

--- dist/mgplus-dom.js
+++ dist/mgplus-dom.js
@@ -1,15 +1,3 @@
-
-function $parcel$defineInteropFlag(a) {
-  Object.defineProperty(a, '__esModule', {value: true, configurable: true});
-}
-
-function $parcel$export(e, n, v, s) {
-  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
-}
-
-$parcel$defineInteropFlag(module.exports);
-
-$parcel$export(module.exports, "default", () => $4fa36e821943b400$export$2e2bcd8739ae039);
 function $2a551a86451a9d86$export$2e2bcd8739ae039() {
     let currentDropdown = null;
     document.querySelectorAll("[data-toggle~=dropdown]").forEach(setupDropdown);
@@ -165,7 +153,7 @@ function $a6a67ad7ed0be81b$export$2e2bcd8739ae039() {
 }
 
 
-function $4fa36e821943b400$export$2e2bcd8739ae039() {
+function $4fa36e821943b400$var$registerDomPlugins() {
     (0, $2a551a86451a9d86$export$2e2bcd8739ae039)();
     (0, $e2ea968a57da8a86$export$2e2bcd8739ae039)();
     (0, $e49cd42f69d2521f$export$2e2bcd8739ae039)();
@@ -173,7 +161,7 @@ function $4fa36e821943b400$export$2e2bcd8739ae039() {
 }
 (function(window1) {
     window1.mgplus = {
-        registerDomPlugins: $4fa36e821943b400$export$2e2bcd8739ae039
+        registerDomPlugins: $4fa36e821943b400$var$registerDomPlugins
     };
 })(window);
 
--- index.js
+++ index.js
@@ -1,40 +1,35 @@
- 
-
 (async function loadContent(link) {
-  //get all imported html files from header 
+  //get all imported html files from header
   await loadDemoSections();
-  console.log(window.mgplus)
-  window.mgplus.registerDomPlugins();
   registerDemoFeatures();
-})(); 
+  window.mgplus.registerDomPlugins();
+})();
 
 async function loadDemoSections() {
   const imports = document.querySelectorAll("link[rel='import']");
-for (let i = 0; i < imports.length; i++) {
-  const link = imports[i];
-  const main = document.querySelector("main"); 
-  const response = await fetch(link.href);
-  const content = await response.text();
- const section = document.createElement("div");  
- section.innerHTML = content;
- main.appendChild(section); 
-  }  
-} 
+  for (let i = 0; i < imports.length; i++) {
+    const link = imports[i];
+    const main = document.querySelector("main");
+    const response = await fetch(link.href);
+    const content = await response.text();
+    const section = document.createElement("div");
+    section.innerHTML = content;
+    main.appendChild(section);
+  }
+}
 
-function registerMgPlugins(){
-
+function registerMgPlugins() {
   registerDropdown();
   registerModals();
   registerNavs();
-  registerTabs(); 
+  registerTabs();
 }
 
-function registerDemoFeatures(){
+function registerDemoFeatures() {
+  const theme = document.querySelector(":root");
 
- const theme = document.querySelector(":root");
-
- //color picker interaction
- const colorPicker = new iro.ColorPicker("#picker", {
+  //color picker interaction
+  const colorPicker = new iro.ColorPicker("#picker", {
     // Set the size of the color picker
     width: 120,
     // Set the initial color to pure red
@@ -50,9 +45,9 @@ function registerDemoFeatures(){
     "click",
     function () {
       var current = document.querySelector("html").getAttribute("data-theme");
-      document.querySelector("html")
-      .setAttribute("data-theme",current == "dark" ? "light" : "dark");      
-       
+      document
+        .querySelector("html")
+        .setAttribute("data-theme", current == "dark" ? "light" : "dark");
     },
     false
   );
@@ -69,38 +64,33 @@ function registerDemoFeatures(){
       }, 2000);
     },
     false
-  ); 
+  );
 
- 
   function prettify(el) {
     function indentHtml(el) {
-
       return format(el, 1).innerHTML;
     }
-    
+
     function format(node, level) {
-    
-    
-      let indentBefore = new Array(level++ + 1).join('  '),
-        indentAfter = new Array(level - 1).join('  '),
+      let indentBefore = new Array(level++ + 1).join("  "),
+        indentAfter = new Array(level - 1).join("  "),
         textNode;
-    
+
       for (let i = 0; i < node.children.length; i++) {
-    
-        textNode = document.createTextNode('' + indentBefore);
+        textNode = document.createTextNode("" + indentBefore);
         node.insertBefore(textNode, node.children[i]);
-    
+
         format(node.children[i], level);
-    
+
         if (node.lastElementChild == node.children[i]) {
-          textNode = document.createTextNode('' + indentAfter);
+          textNode = document.createTextNode("" + indentAfter);
           node.appendChild(textNode);
         }
       }
-    
+
       return node;
     }
-  
+
     function htmlEscape(s) {
       return s
         .replace(/\n\n/g, "")
@@ -111,22 +101,20 @@ function registerDemoFeatures(){
     }
 
     var elSource = document.getElementById(el.getAttribute("data-source"));
-   if (elSource)
-   {
-    // this page's own source code
-    indentHtml(elSource);
-    var quineHtml = htmlEscape(elSource.outerHTML);
+    if (elSource) {
+      // this page's own source code
+      indentHtml(elSource);
+      var quineHtml = htmlEscape(elSource.outerHTML);
 
-    // Highlight the operative parts:
-    quineHtml = quineHtml.replace(
-      /&lt;script src[\s\S]*?&gt;&lt;\/script&gt;|&lt;!--\?[\s\S]*?--&gt;|&lt;pre\b[\s\S]*?&lt;\/pre&gt;/g,
-      '<span class="operative">$&</span>'
-    );
-    // insert into PRE
-    el.innerHTML = quineHtml;
-    
+      // Highlight the operative parts:
+      quineHtml = quineHtml.replace(
+        /&lt;script src[\s\S]*?&gt;&lt;\/script&gt;|&lt;!--\?[\s\S]*?--&gt;|&lt;pre\b[\s\S]*?&lt;\/pre&gt;/g,
+        '<span class="operative">$&</span>'
+      );
+      // insert into PRE
+      el.innerHTML = quineHtml;
+    }
   }
- }
   document.querySelectorAll("[data-toggle~=prettify]").forEach(prettify);
-  prettyPrint(); 
+  prettyPrint();
 }
